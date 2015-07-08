4ab8de993a14c8378b9b1ac1d47af748bdf35d56
Balint Erdi <balint.erdi@gmail.com>
2015-07-08 14:05:59 +0200

    Add test to see if setAction is called on click

--- tests/integration/components/star-rating-test.js
+++ tests/integration/components/star-rating-test.js
@@ -28,3 +28,20 @@ test('Renders the full and empty stars correctly', function(assert) {
   assert.equal(this.$('.glyphicon-star-empty').length, 8, "The right amount of empty stars is rendered after changing rating");
 });
 
+test('Triggers the passed-in action handler', function(assert) {
+  assert.expect(1);
+
+  var song = Ember.Object.create({ rating: 4 }),
+      clickedRating;
+
+  this.set('song', song);
+  this.set('maxRating', 5);
+  this.on("updateRating", function(rating) {
+    clickedRating = rating;
+  });
+
+  this.render(hbs`{{star-rating item=song rating=song.rating setAction="updateRating"}}`);
+  this.$('.star-rating').click();
+
+  assert.ok(clickedRating, "The `updateRating` action was called");
+});
